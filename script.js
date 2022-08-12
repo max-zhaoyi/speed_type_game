abd1f5a6ffd48ddd6a26348b00562acba64653ee
Zhao Yi (Max) Pi <max.zhaoyi@gmail.com>
2022-08-12 16:43:04 -0400

    done tutorial
    next implement react elements and play with css

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
 const RANDOM_QUOTES_API_URL = 'http://api.quotable.io/random'
 const quoteDisplayElement = document.getElementById('quote-display')
 const quoteInputElement = document.getElementById('quote-input')
+const timerElement = document.getElementById('type-timer')
+let startTime
 
 quoteInputElement.addEventListener('input', () => {
     const arrayQuote = quoteDisplayElement.querySelectorAll('span')
     const arrayValue = quoteInputElement.value.split('')
+    let correct = true
     arrayQuote.forEach((characterSpan,index)=>{
         const character = arrayValue[index]
         if (character == null){
             characterSpan.classList.remove('correct')
             characterSpan.classList.remove('incorrect')
-        } else if(character === characterSpan){
+            correct = false
+        } else if(character === characterSpan.innerText){
             characterSpan.classList.add('correct')
             characterSpan.classList.remove('incorrect')
         } else {
             characterSpan.classList.remove('correct')
             characterSpan.classList.add('incorrect')
+            correct = false
         }
     })
+    if (correct) renderNextQuote()
 })
 
 function generateQuote(){
@@ -26,16 +32,29 @@ function generateQuote(){
     .then(data => data.content)
 }
 
+
+function startTimer(){
+    timerElement.innerText = 0
+    startTime = new Date()
+    setInterval (()=> {
+        timerElement.innerText = refreshTime()
+    }, 1000)
+}
+
+function refreshTime() {
+    return Math.floor((new Date() - startTime)/1000)
+}
+
 async function renderNextQuote(){
     const quote = await generateQuote()
-    quoteDisplayElement.innerText = quote
+    quoteDisplayElement.innerHTML = ''
     quote.split('').forEach(character => {
         const characterSpan = document.createElement('span')
-        characterSpan.classList.add('correct')
         characterSpan.innerText = character
         quoteDisplayElement.appendChild(characterSpan)
     })
     quoteInputElement.value = null
+    startTimer()
 }
 
 renderNextQuote()
